e1544f260626a64bc408497623ea89c58664239f
Margaret Andoh <andoh26@gmail.com>
2023-10-06 17:24:00 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char alp[26] + "abcdefghijklmnopqrstuvwxyz";
-	int i:
-	
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
