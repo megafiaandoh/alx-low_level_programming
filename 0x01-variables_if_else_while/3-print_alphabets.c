fe8d9bc16ae2a8260c45211a3e02a920f4cc8958
Margaret Andoh <andoh26@gmail.com>
2023-10-06 18:03:52 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
+ * main - Entry point.
+ * Description: 'print alphabets lowercase and uppercase
+ * Return: Always 0 
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      int i;
+	int n = 97;
+	int m = 65;
 
-      for(i = 0; i < 52, i++)
-      {
-	      putchar(alp[i]);
-      }
-      putchar('\n');
-      return (0);
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++
+	}
+	putchar('\n');
+	return (0);
 }
