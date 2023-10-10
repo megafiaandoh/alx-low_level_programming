13763c2cabc0c7999d17da57f75ca1d675d9e9d3
Margaret Andoh <andoh26@gmail.com>
2023-10-10 08:38:42 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - all alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+	
+	for (alpha ='a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	
+	_putchar('\n');
+}
