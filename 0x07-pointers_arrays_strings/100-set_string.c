8942ad43a666f71e1f8e1c02952a78d4fb423295
Margaret Andoh <andoh26@gmail.com>
2023-10-23 08:33:50 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function to be used
+ * @s: parameter to be used
+ * @to: parameter to be used
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
