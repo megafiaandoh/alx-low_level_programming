7bbefc23bd5d0119ad36311556986f447b869f39
Margaret Andoh <andoh26@gmail.com>
2023-10-25 09:55:56 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * sqrt_a - returns the natural square root of a number
+ * @a: parameter to be
+ * @b: parameter to be used
+ * Return: resulting square root or -1
+ */
+
+int sqrt_a(int a, int b)
+
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > 4)
+	{
+		return (-1);
+	}
+	return (sqrt_a(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root
+ * @n: input number
+ * Return: natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt_a(n, 0));
+}
