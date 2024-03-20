b02369f03e9ed1ee1143e84bf4bdbdacbb4d47f9
megafiaandoh <andoh26@gmail.com>
2024-03-20 08:07:16 -0700

    makefiles

--- 0x1C-makefiles/tests/5-main.py
+++ 0x1C-makefiles/tests/5-main.py
@@ -2,7 +2,7 @@
 """
 5-main
 """
-island_perimeter = __import__('5-island_perimeter').island_perimete
+island_perimeter = __import__('5-island_perimeter').island_perimeter
 
 if __name__ == "__main__":
     grid = [
